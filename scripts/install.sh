b26d403816c1b5d69a1df4bb0db45e355f6f6f2a
Maxime Gabut <maxime.gabut@gmail.com>
2017-06-30 22:19:46 +0200

    Add google authentication
    For now both are activated at the same time, which does not make that
    much sense in a "real" environment IMO, but it's helpfull in dev since
    it allows to have multiple user for test without the need to create fake
    github accounts.

--- scripts/install.sh
+++ scripts/install.sh
@@ -42,10 +42,14 @@ echo ''
 
 echo 'Setting up configuration' :
 	set_in_gerrit_config gerrit.canonicalWebUrl ${GERRIT_CANONIAL_WEB_URL}
-	# authentication with github
+	# authentication oauth
 	set_in_gerrit_config auth.type OAUTH
 	set_in_secure_config auth.gitBasicAuthPolicy HTTP
+	# authentication with github
 	set_in_gerrit_config plugin.gerrit-oauth-provider-github-oauth.client-id ${OAUTH_GITHUB_CLIENT_ID}
 	set_in_secure_config plugin.gerrit-oauth-provider-github-oauth.client-secret ${OAUTH_GITHUB_CLIENT_SECRET}
+	# authentication with google
+	set_in_gerrit_config plugin.gerrit-oauth-provider-google-oauth.client-id ${OAUTH_GOOGLE_CLIENT_ID}
+	set_in_secure_config plugin.gerrit-oauth-provider-google-oauth.client-secret ${OAUTH_GOOGLE_CLIENT_SECRET}
 
 echo ' Done.'
