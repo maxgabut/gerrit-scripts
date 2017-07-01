359aca0bacb22440e39417c68fdcd0dc6e3bbfda
Maxime Gabut <maxime.gabut@gmail.com>
2017-07-01 18:01:17 +0200

    Factorise resource fetching script

--- scripts/fetch-resources.sh
+++ scripts/fetch-resources.sh
@@ -4,29 +4,24 @@ set -e
 cd $(dirname $0)
 
 source variables
+source checks
 
-echo 'Getting gerrit war :'
-if [ -f ${GERRIT_WAR_LOCAL_PATH} ]; then
-	echo '  file already there, moving on.'
-else
-	echo '  file absent, fetching it...'
+function download_file() {
+	[ $# -eq 0 ] && fail_for_missing_arg || source_url=${1}; shift;
+	[ $# -eq 0 ] && fail_for_missing_arg || dest_path=${1}; shift;
+
+	echo "Downloading ${source_url}:"
+	if [ -f ${dest_path} ]; then
+		echo ' file already there, moving on.'
+	else
+		echo ''
+		wget ${source_url} --output-document ${dest_path}
+		echo '  Done.'
+	fi
+
 	echo ''
-	wget $GERRIT_WAR_URL --output-document $GERRIT_WAR_LOCAL_PATH
-	echo '  Done.'
-fi
-echo ''
+}
 
-
-echo 'Getting oauth plugin jar :'
-if [ -f ${OAUTH_PLUGIN_LOCAL_PATH} ]; then
-	echo '  file already there, moving on.'
-else
-	echo '  file absent, fetching it...'
-	echo ''
-	wget $OAUTH_PLUGIN_JAR_URL --output-document $OAUTH_PLUGIN_LOCAL_PATH
-	echo '  Done.'
-fi
-echo ''
-
-
+download_file ${GERRIT_WAR_URL} ${GERRIT_WAR_LOCAL_PATH}
+download_file ${OAUTH_PLUGIN_JAR_URL} ${OAUTH_PLUGIN_LOCAL_PATH}
 
