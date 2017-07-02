03c166cde67fd629eadddf80b90e8b2a3f7f814f
Maxime Gabut <maxime.gabut@gmail.com>
2017-07-02 14:39:27 +0200

    Extract initialization out of install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 
 source variables
 
-./fetch-resources.sh
+./initialise.sh
 
 set_in_gerrit_config() {
 	git config -f "${SITE_PATH}/etc/gerrit.config" "$@"
@@ -14,31 +14,6 @@ set_in_gerrit_config() {
 set_in_secure_config() {
 	git config -f "${SITE_PATH}/etc/secure.config" "$@"
 }
-
-echo 'Initializing review site' :
-if [ -f "${SITE_INIT_FLAG}" ]; then
-	echo '  site already initialized, moving on.'
-else
-	echo '  not initialized, doing it...'
-	echo ''
-
-	./delete.sh
-	mkdir -p "${SITE_PATH}"
-	mkdir -p "${PLUGIN_PATH}"
-	touch "${SITE_INIT_FLAG}"
-
-	cp "${OAUTH_PLUGIN_LOCAL_PATH}" "${PLUGIN_PATH}"/
-
-	java -jar "${GERRIT_WAR_LOCAL_PATH}" init \
-		--batch \
-		--no-auto-start \
-		--install-plugin download-commands \
-		-d "${SITE_PATH}"
-
-	echo ''
-	echo ' Done.'
-fi
-echo ''
 
 echo 'Setting up configuration' :
 	set_in_gerrit_config gerrit.canonicalWebUrl "${GERRIT_CANONIAL_WEB_URL}"
