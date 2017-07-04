b540759e9a1f500ce5aaa2437aff0deaff407230
Maxime Gabut <maxime.gabut@gmail.com>
2017-07-04 22:01:04 +0200

    Add a script for interactive install

---
+++ scripts/initialise-interactive.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+source variables
+./copy-resources.sh
+
+echo 'Initializing INTERACTIVELY review site:'
+if [ -f "${SITE_INIT_FLAG}" ]; then
+	echo '  site already initialized, moving on.'
+else
+	echo '  not initialized, doing it...'
+	echo ''
+
+	java -jar "${GERRIT_WAR_LOCAL_PATH}" init -d "${SITE_PATH}"
+
+	touch "${SITE_INIT_FLAG}"
+
+	echo ''
+	echo ' Done.'
+fi
+echo ''
